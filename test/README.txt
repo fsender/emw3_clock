e7de67f751f106f87e3ddf68e3eed83c43b72103
fsender <f_ender@163.com>
2021-11-20 16:57:46 +0800

    update Hardware Test

--- test/README.txt
+++ test/README.txt
@@ -9,3 +9,126 @@ in the development cycle.
 
 More information about PlatformIO Unit Testing:
 - https://docs.platformio.org/page/plus/unit-testing.html
+
+ * 智能电子墨水时钟 emw3
+ 
+ * ----------|\      /|
+ *  \        | \    / |
+ *   >       |  \  /  |
+ *  /        |   \/   |
+ * ----------|        |
+ * |        |----------
+ * |   /\   |        /
+ * |  /  \  |       <
+ * | /    \ |        \
+ * |/      \|----------
+ * 重要开发事项: 
+ * 由于最新版软件和硬件原因,SD卡不再支持热插拔
+ * 使用前,请修改 ESP8266SdFat 库中 ENABLE_DEDICATED_SPI 的值为 0
+ * 不要使用 clearScreen() 函数, 请使用 fillScreen() 函数, 然后 performDisplay();
+
+以下代码将会被emw3库使用,请在任何程序中都要包含以下代码.
+注意最后一个 busy 引脚的参数为 -1, 因为我们使用中断来检测此功能
+ GxEPD2_BW<GxEPD2_213, GxEPD2_213::HEIGHT> 
+epd(GxEPD2_213(/*CS*/ _DEFINA_EPD_CS_PIN, /*DC*/ _DEFINA_EPD_DC_PIN,
+/*RST*/ _DEFINA_EPD_RST_PIN, /*BUSY*/ -1));
+
+U8G2_FOR_ADAFRUIT_GFX cn;
+
+
+
+以下是我的设置方案
+Below is my settings-----
+第一步: 前往 GxEPD2_BW.cpp文件的111行, 然后用以下代码替换原来的switch代码
+Step 1: Go to file GxEPD2_BW.h line 111:
+replace to the code below:
+      switch (getRotation())
+      {
+        case 1:
+          _swap_(x, y);
+          x = 121 - x;
+          break;
+        case 2:
+          x = 121 - x;
+          y = HEIGHT - y - 1;
+          break;
+        case 3:
+          _swap_(x, y);
+          y = HEIGHT - y - 1;
+          break;
+      }
+第二步: 前往 GxEPD2_213.cpp文件的322行, 然后用以下代码替换原来的代码
+Step 2: Go to file GxEPD2_213.cpp line 322:
+replace to the code below:
+
+void GxEPD2_213::_InitDisplay()
+{
+  if (_hibernating) _reset();
+  _writeCommand(0x01); // Panel configuration, Gate selection
+  _writeData((HEIGHT - 1) % 256);
+  _writeData((HEIGHT - 1) / 256);
+  _writeData(0x00);
+  _writeCommand(0x03); // Set Gate driving voltage
+  _writeData(0x15);
+  _writeData(0x0e);
+  _writeCommand(0x04); // Set Source output voltage.
+  _writeData(0x1d);
+
+  _writeCommand(0x0c); // softstart
+  _writeData(0xd7);
+  _writeData(0xd6);
+  _writeData(0x9d);
+  _writeCommand(0x2d); // VCOM setting
+  _writeData(0x55);    // * different
+  _writeCommand(0x3a); // DummyLine
+  _writeData(0x1a);    // 4 dummy line per gate
+  _writeCommand(0x3b); // Gatetime
+  _writeData(0x08);    // 2us per line
+  _setPartialRamArea(0, 0, WIDTH, HEIGHT);
+}
+const uint8_t GxEPD2_213::LUTDefault_full[] PROGMEM = { 0x32,  // command
+  0x22, 0x55, 0xaa, 0x11, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x0f, 0x0f, 0x11, 0x0e, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+const uint8_t GxEPD2_213::LUTDefault_part[] PROGMEM = { 0x32,  // command
+  0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x0d, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  };
+
+第三步: 前往 Adafruit_GFX.cpp文件的大约111行, 找到Adafruit_GFX::Adafruit_GFX(w, h)函数, 
+然后用以下代码替换原来的代码 (行数不同因为库版本不同)
+Step 3: Go to file Adafruit_GFX.cpp around line 111:
+Go to function Adafruit_GFX::Adafruit_GFX(int16_t w, int16_t h)
+replace to the code below: (different version of libraries have different position.)
+
+from raw:  _width = WIDTH;
+to------>  _width = WIDTH - 6;
+
+
+第四步: 前往 Adafruit_GFX.cpp文件的大约1316行, 找到Adafruit_GFX::setRotation(uint8_t x)函数, 
+然后用以下代码替换原来的代码
+Step 4: Go to file Adafruit_GFX.cpp around line 1316:
+Go to function Adafruit_GFX::setRotation(uint8_t x)
+replace to the code below: (different version of libraries have different position.)
+
+void Adafruit_GFX::setRotation(uint8_t x) {
+  rotation = (x & 3);
+  switch (rotation) {
+  case 0:
+  case 2:
+    _width = WIDTH - 6;
+    _height = HEIGHT;
+    break;
+  case 1:
+  case 3:
+    _width = HEIGHT;
+    _height = WIDTH - 6;
+    break;
+  }
+}
+第五步: 前往文件 GxEPD2_213.h, 行30, 31:
+把后面的两个初始值修改为0
+Step 5: Go to file GxEPD2_213.h line 30 to 31:
+Set these two constants to 0 :
+
+    static const uint16_t full_refresh_time = 0; // ms, e.g. 3883686us
+    static const uint16_t partial_refresh_time = 0; // ms, e.g. 268173us
---
+++ test/hardwareTest2/hardwareTest2.cpp
@@ -0,0 +1,221 @@
+/************* F R I E N D S H I P E N D E R **************
+ * @file main.c
+ * 智能电子墨水时钟 emw3
+ * 使用前,请修改 ESP8266SdFat 库中 ENABLE_DEDICATED_SPI 的值为 0
+ * ----------|\      /|
+ *  \        | \    / |
+ *   >       |  \  /  |
+ *  /        |   \/   |
+ * ----------|        |
+ * |        |----------
+ * |   /\   |        /
+ * |  /  \  |       <
+ * | /    \ |        \
+ * |/      \|----------
+  在开发之前请仔细参考 readme.txt
+ */
+#include <Arduino.h>
+#include <SPI.h>
+#include <FS.h>
+#include <SD.h>
+#include <ESP8266SDUpdater.h>
+#include <GxEPD2_BW.h>
+#include "emw3.h"
+GxEPD2_BW<GxEPD2_213, GxEPD2_213::HEIGHT> 
+epd(GxEPD2_213(/*CS*/ _DEFINA_EPD_CS_PIN, /*DC*/ _DEFINA_EPD_DC_PIN,
+/*RST*/ _DEFINA_EPD_RST_PIN, /*BUSY*/ -1));
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <U8g2_for_Adafruit_GFX.h>
+
+emw3_base emw3;
+U8G2_FOR_ADAFRUIT_GFX cn;
+bool sd_in =0;
+void setup(){
+  Serial.begin(115200);
+  Serial.println("\nInput new lut data.");
+  emw3.begin(NULL);
+  epd.fillScreen(GxEPD_WHITE);
+  emw3.blockedDisplay(0);
+  if(!SD.begin(_DEFINA_SD_CS_PIN)){
+    sd_in = 0;
+    Serial.println("SD not available!");
+    digitalWrite(D4,LOW);
+    delay(500);
+    digitalWrite(D4,HIGH);
+  }
+  else sd_in =1;
+  epd.setFont(&FreeMonoBold9pt7b);
+  pinMode(D4,OUTPUT);
+  digitalWrite(D4,HIGH);
+  delay(1000);
+  digitalWrite(D4,LOW);
+  delay(20);
+  digitalWrite(D4,HIGH);
+  uint8_t r=0;
+  if(ESP.getResetReason() == "Deep-Sleep Wake"){
+    r=1;
+  }
+  Serial.println("EPD init done.");
+    epd.fillScreen(GxEPD_WHITE);
+    epd.setCursor(10,20);
+    if(r) epd.print("Hibernate Wake up!");
+    else epd.print("Hello EMW3!");
+    epd.setCursor(10,40);
+    epd.print("EMW3 Hardware Test");
+    epd.setCursor(10,60);
+    if(sd_in) {
+      epd.print("SD card init done.");
+      epd.setCursor(10,80);
+      epd.print("Press K1 to loadback.");
+    }
+    else epd.print("Insert SD card.");
+    cn.setCursor(10,114);
+    cn.print("你好,世界,你好,墨水时钟");
+  emw3.performDisplay(0);
+  for(int i=0;i<100;i++) {
+    Serial.println();
+    delay(50);
+  }
+  Serial.print("Press to load.");
+    digitalWrite(D4,LOW);
+    delay(20);
+    digitalWrite(D4,HIGH);
+  delay(980);
+  if(emw3.getBtn(_DEFINA_BtnL)==0 && sd_in){
+    epd.init(0,false);
+      epd.setCursor(40,75);
+      epd.print("Loading MENU.BIN");
+    emw3.performDisplay(1);
+    for(int i=0;i<20;i++) {
+      Serial.println(digitalRead(_DEFINA_EPD_BUSY_PIN));
+      delay(50);
+    }
+    if(SD.begin(_DEFINA_SD_CS_PIN)) updateFromSD();
+    ESP.restart();
+  }
+  
+  Serial.print(".");
+    digitalWrite(D4,LOW);
+    delay(20);
+    digitalWrite(D4,HIGH);
+  delay(480);
+  Serial.println(".");
+}
+
+void loop(){
+  for(int i=0;i<4;i++){
+    epd.setRotation(i);
+    epd.setPartialWindow(0,0,epd.width(),epd.height());
+      epd.fillScreen(GxEPD_WHITE);
+      epd.fillRect(0,0,20,20,GxEPD_BLACK);
+      epd.drawRect(epd.width()-20,0,20,20,GxEPD_BLACK);
+      epd.drawRect(0,epd.height()-20,20,20,GxEPD_BLACK);
+      epd.drawRect(epd.width()-20,epd.height()-20,20,20,GxEPD_BLACK);
+      epd.setCursor(10,50);
+      epd.print("Rotation ");
+      epd.print(i);
+    emw3.performDisplay(1);
+    for(int i=0;i<20;i++) {
+      Serial.println(digitalRead(_DEFINA_EPD_BUSY_PIN));
+      delay(50);
+    }
+  }
+
+    epd.fillScreen(GxEPD_WHITE);
+    epd.fillTriangle(10,10,20,20,15,30,GxEPD_BLACK);
+    epd.setCursor(10,40);
+    epd.print("EPD busy trigger test");
+    Serial.println("Full display....");
+    
+    emw3.performDisplay(0);
+    //此for循环也可以用检测EPD_BUSY的电平实现定时显示
+    for(int i=0;i<20;i++) {
+      Serial.print(digitalRead(_DEFINA_EPD_BUSY_PIN));
+      delay(50);
+    }
+    
+    Serial.println("\nFull display done!!!!");
+    for(int i = 0;i<epd.width();i++){
+      epd.drawFastVLine(i,0,i%100+22,GxEPD_BLACK);
+      emw3.performDisplay(1);
+      delay(10);
+    }
+    emw3.blockedDisplay(1);
+    while(digitalRead(_DEFINA_EPD_BUSY_PIN)==1) { //等待显示刷新完成...
+      yield();
+    }
+    delay(1000);
+  for(int times=0;times<4;times++){
+  uint8_t btnlstate,btnmstate,btnrstate,sdstate=0;
+  btnlstate=emw3.getBtn(_DEFINA_BtnL);
+  btnmstate=emw3.getBtn(_DEFINA_BtnM);
+  btnrstate=emw3.getBtn(_DEFINA_BtnR);
+  //if(sd_in) {
+    sdstate=SD.begin(_DEFINA_SD_CS_PIN);
+    sdstate += SD.exists("/MENU.BIN");
+  //}
+  //else sdstate=255;
+  Serial.print("btnmstate ");
+  Serial.println(btnlstate);
+  Serial.print("btnmstate ");
+  Serial.println(btnmstate);
+  Serial.print("btnrstate ");
+  Serial.println(btnrstate);
+  Serial.print("sdstate ");
+  Serial.println(sdstate);
+  epd.setRotation(1);
+  if(times==0) {
+    epd.fillScreen(GxEPD_WHITE);
+    epd.fillTriangle(10,10,20,20,15,30,GxEPD_BLACK);
+    Serial.println("Full display....");
+    emw3.blockedDisplay(0);
+    Serial.println("Full display done!!!!");
+  }
+    epd.fillScreen((times&1)?GxEPD_WHITE:GxEPD_BLACK);
+    epd.setTextColor((times&1)?GxEPD_BLACK:GxEPD_WHITE);
+    epd.setCursor(10,20);
+    epd.print("Simple Test");
+    epd.setCursor(10,40);
+    epd.print("Button L(K1) State: ");
+    epd.print(btnlstate);
+    epd.setCursor(10,60);
+    epd.print("Button M(K2) State: ");
+    epd.print(btnmstate);
+    epd.setCursor(10,80);
+    epd.print("Button R(K3) State: ");
+    epd.print(btnrstate);
+    epd.setCursor(10,100);
+    epd.print("SD File exists:");
+    epd.print(sdstate);
+    emw3.blockedDisplay(1);
+    delay(1000);
+    for(int i=0;i<20;i++) {
+      Serial.println(digitalRead(_DEFINA_EPD_BUSY_PIN));
+      delay(50);
+    }
+  Serial.println(times);
+  }
+  delay(500);
+  epd.setFullWindow();
+  File hello = SD.open("/hello.txt");
+  String hellostr = "No data";
+  if(hello) {
+    hellostr = hello.readString();
+    hello.close();
+  }
+    epd.fillScreen(GxEPD_WHITE);
+    epd.setCursor(20,20);
+    epd.print("hibernating");
+    epd.setCursor(20,40);
+    epd.print(hellostr);
+    emw3.performDisplay(0);
+    for(int i=0;i<100;i++) {
+      Serial.println(digitalRead(_DEFINA_EPD_BUSY_PIN));
+      delay(50);
+    }
+  epd.hibernate();
+    digitalWrite(D4,LOW);
+    delay(200);
+    digitalWrite(D4,HIGH);
+  ESP.deepSleep(10000000);
+}
